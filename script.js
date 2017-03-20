8bd8ee26c8c78d6b409976a5b4e80bc3cff7ddb7
root <root@localhost.localdomain>
2017-03-20 05:11:07 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var x = document.getElementById("text").value;
+  
+    document.getElementById("enteredText").innerHTML = x;
+}
+
+function functionOne() {
+	
+	var count = 0;
+	
+	var i = 0;
+	
+	document.getElementById("stats").innerHTML = count;
+    
+    function countItems (text){
+      var counter = a;
+      
+      for (count = 0; count< text.length; count++) {
+        counter ++;
+      }
+    }
+}
